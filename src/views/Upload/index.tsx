0d79feab8f5a057873a1ce3a2bd405f454d86e65
Thomas Zarebczan <thomas.zarebczan@gmail.com>
2024-06-21 14:12:55 -0400

    Support for upgraded profiles
    1) Update Asset and Collection sources to above
    2) Change Creator to profile id in collection (178)
    3) Add ProfileVersion to support new handlers/assignment
    4) Pass quantity on Asset upload (used later for Assignment)

--- src/views/Upload/index.tsx
+++ src/views/Upload/index.tsx
@@ -11,6 +11,7 @@ import { Loader } from 'components/atoms/Loader';
 import { Modal } from 'components/molecules/Modal';
 import { AssetsTable } from 'components/organisms/AssetsTable';
 import {
+	AO_VERSIONS,
 	AOS,
 	CONTENT_TYPES,
 	DEFAULT_UCM_BANNER,
@@ -177,8 +178,7 @@ export default function Upload() {
 
 			const collectionTags: TagType[] = [
 				{ name: TAGS.keys.contentType, value: CONTENT_TYPES.json },
-				{ name: TAGS.keys.creator, value: arProvider.walletAddress },
-				{ name: TAGS.keys.profileCreator, value: arProvider.profile.id },
+				{ name: TAGS.keys.creator, value: arProvider.profile.id },
 				{
 					name: TAGS.keys.ans110.title,
 					value: uploadReducer.data.title,
@@ -194,6 +194,7 @@ export default function Upload() {
 					value: uploadReducer.data.title,
 				},
 				{ name: 'Action', value: 'Add-Collection' },
+				{ name: 'ProfileVersion', value: AO_VERSIONS.profileVersion },
 			];
 
 			if (bannerTx) collectionTags.push({ name: TAGS.keys.banner, value: bannerTx });
@@ -297,6 +298,7 @@ export default function Upload() {
 						tags: [
 							{ name: 'Action', value: 'Add-Collection-To-Profile' },
 							{ name: 'ProfileProcess', value: arProvider.profile.id },
+							{ name: 'ProfileVersion', value: AO_VERSIONS.profileVersion },
 						],
 					});
 
@@ -345,7 +347,9 @@ export default function Upload() {
 						{ name: TAGS.keys.ans110.type, value: type },
 						{ name: TAGS.keys.ans110.implements, value: TAGS.values.ansVersion },
 						{ name: TAGS.keys.dateCreated, value: dateTime },
+						{ name: 'Quantity', value: balance.toString() },
 						{ name: 'Action', value: 'Add-Uploaded-Asset' },
+						{ name: 'ProfileVersion', value: AO_VERSIONS.profileVersion },
 					];
 
 					uploadReducer.data.topics.forEach((topic: string) =>
@@ -446,6 +450,7 @@ export default function Upload() {
 									{ name: 'Action', value: 'Add-Asset-To-Profile' },
 									{ name: 'ProfileProcess', value: arProvider.profile.id },
 									{ name: 'Quantity', value: balance.toString() },
+									{ name: 'ProfileVersion', value: AO_VERSIONS.profileVersion },
 								],
 								data: JSON.stringify({ Id: processId, Quantity: balance }),
 							});
